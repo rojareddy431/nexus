84cf91c52ab50a4a5ea24813be571e9603757771
ROJA <=>
2017-12-06 16:02:43 -0500

    commiting

---
+++ src/main/java/veridic/App.java
@@ -0,0 +1,13 @@
+package veridic;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
